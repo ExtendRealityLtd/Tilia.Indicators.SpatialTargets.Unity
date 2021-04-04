e3e6630724dcd2108b77802be6a9806ee3223082
Harvey Ball <me@harveyball.com>
2021-04-04 14:30:10 +0100

    feat(Utility): add prefab creator
    The latest version of Zinnia has the basis of a prefab creator that can
    be used to enable easy adding of prefabs to a scene without needing to
    drag and drop from directories. Instead a new menu item is added for
    quickly adding prefabs. The guide has been updated to accommodate this
    and the FodyWeavers.xml is now located in the root to serve both the
    Runtime and Editor scripts.

---
+++ Editor/Utility/PrefabCreator.cs
@@ -0,0 +1,46 @@
+namespace Tilia.CameraRigs.TrackedAlias.Utility
+{
+    using System.IO;
+    using UnityEditor;
+    using Zinnia.Utility;
+
+    public class PrefabCreator : BasePrefabCreator
+    {
+        private const string group = "Tilia/";
+        private const string project = "Indicators/SpatialTargets/";
+        private const string menuItemRoot = topLevelMenuLocation + group + subLevelMenuLocation + project;
+
+        private const string package = "io.extendreality.tilia.indicators.spatialtargets.unity";
+        private const string baseDirectory = "Runtime";
+        private const string prefabDirectory = "Prefabs";
+        private const string prefabSuffix = ".prefab";
+
+        private const string prefabSpatialTargetsDispatcher = "Indicators.SpatialTargets.Dispatcher";
+        private const string prefabSpatialTargetsProcessor = "Indicators.SpatialTargets.Processor";
+        private const string prefabSpatialTargetsTarget = "Indicators.SpatialTargets.Target";
+
+        [MenuItem(menuItemRoot + prefabSpatialTargetsDispatcher, false, priority)]
+        private static void AddSpatialTargetsDispatcher()
+        {
+            string prefab = prefabSpatialTargetsDispatcher + prefabSuffix;
+            string packageLocation = Path.Combine(packageRoot, package, baseDirectory, prefabDirectory, prefab);
+            CreatePrefab(packageLocation);
+        }
+
+        [MenuItem(menuItemRoot + prefabSpatialTargetsProcessor, false, priority)]
+        private static void AddSpatialTargetsProcessor()
+        {
+            string prefab = prefabSpatialTargetsProcessor + prefabSuffix;
+            string packageLocation = Path.Combine(packageRoot, package, baseDirectory, prefabDirectory, prefab);
+            CreatePrefab(packageLocation);
+        }
+
+        [MenuItem(menuItemRoot + prefabSpatialTargetsTarget, false, priority)]
+        private static void AddSpatialTargetsTarget()
+        {
+            string prefab = prefabSpatialTargetsTarget + prefabSuffix;
+            string packageLocation = Path.Combine(packageRoot, package, baseDirectory, prefabDirectory, prefab);
+            CreatePrefab(packageLocation);
+        }
+    }
+}
